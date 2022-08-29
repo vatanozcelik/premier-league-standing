8709b2da7cedb3000364e2b159e3aa19dbbd20a6
vatanozcelik <vatanozcelik91@gmail.com>
2022-08-29 16:02:07 +0300

    django rest framework is added

--- middledjango/settings.py
+++ middledjango/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # apps
+    'core.apps.CoreConfig',
+
+    # 3rd party libraries
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
