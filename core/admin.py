73f2fa9e0f2676205e7412a605967fe06ca6de99
vatanozcelik <vatanozcelik91@gmail.com>
2022-08-31 15:13:28 +0300

    team, league, and footballer tables are registered among admin site

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from core.models import Footballer, League, Team
+
 # Register your models here.
+
+admin.site.register(Footballer)
+admin.site.register(League)
+admin.site.register(Team)
+
