d966087c895795bac037109ace0b23aeecc30495
vatan ozcelik <vatanozcelik91@gmail.com>
2022-09-07 23:57:33 +0300

    space

--- middledjango/settings.py
+++ middledjango/settings.py
@@ -139,4 +139,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'css/images')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField' 
