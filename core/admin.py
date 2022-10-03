4ff7949e05921d44fedf392a92f16b3cd6251d43
vatan ozcelik <vatanozcelik91@gmail.com>
2022-10-03 19:08:16 +0300

    premier league teams have added

--- core/admin.py
+++ core/admin.py
@@ -3,6 +3,17 @@ from django.contrib import admin
 from core.models import Footballer, League, Team
 
 
+class FootballerAdmin(admin.ModelAdmin):
+    list_display = (
+        'player',
+        'team'
+    )
+    search_fields = (
+        'team',
+        'player'
+    )
+
+
 class TeamAdmin(admin.ModelAdmin):
     search_fields = (
         'name',
@@ -21,6 +32,6 @@ class LeagueAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
 
 
-admin.site.register(Footballer)
+admin.site.register(Footballer, FootballerAdmin)
 admin.site.register(League, LeagueAdmin)
 admin.site.register(Team, TeamAdmin)
